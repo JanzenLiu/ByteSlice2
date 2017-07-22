4365ff202b1e58d3ea6cfd9043cfdca2c910447a
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-22 15:40:13 +0800

    test the correctness of ScanByte function added

---
+++ experiments/bytewise_scan.cpp
@@ -0,0 +1,40 @@
+#include    <ctime>
+
+#include    "include/common.h"
+#include    "include/types.h"
+#include    "include/column_block.h"
+#include    "include/bitvector_block.h"
+
+using namespace byteslice;
+
+int main(){
+	size_t num_tuples = 64;
+	double selectivity = 0.3;
+	Comparator comp = Comparator::kLess;
+
+	ByteSliceColumnBlock col_block = new ByteSliceColumnBlock<12>();
+	ByteSliceColumnBlock byte_block = new ByteSliceColumnBlock<8>()
+	BitVectorBlock bv_block1 = new BitVectorBlock(num_tuples);
+	BitVectorBlock bv_block2 = new BitVectorBlock(num_tuples);
+
+	const WordUnit mask = (1ULL << 12) - 1;
+	WordUnit literal = static_cast<WordUnit>(mask * selectivity);
+	WordUnit byte_literal = literal >> 8;
+
+	std::srand(std::time(0));
+    for(size_t i = 0; i < num_tuples; i++){
+        WordUnit code = std::rand() & mask;
+        ByteUnit byte = FLIP(static_cast<ByteUnit>(literal >> 8));
+        col_block->SetTuple(i, code);
+        byte_block->setTuple(i, byte);
+    }
+
+    bv_block1->SetOnes();
+    bv_block2->SetOnes();
+
+    col_block->ScanByte(comp, literal, 0, bv_block1, Bitwise::kSet);
+    byte_block->Scan(comp, byte_literal, bv_block2, Bitwise::kSet);
+
+    std::cout << "ScanByte: " << bv_block1 << std::endl;
+    std::cout << "Scan    : " << bv_block2 << std::endl;
+}

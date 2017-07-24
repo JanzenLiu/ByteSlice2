44bcb27ee4576a1b48560b2e7ae75915e9f7931d
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-25 02:00:44 +0800

    attempt to fix error

--- src/bytewise_scan.cpp
+++ src/bytewise_scan.cpp
@@ -108,7 +108,8 @@ void BytewiseScan::Scan(BitVector* bitvector){
 		// std::cout << "Number of Padding Bits: " << num_bits_shift << std::endl; 
 
 		for(size_t byte = 0; byte < num_bytes[col]; byte++){
-			ByteUnit lit_byte = FLIP(static_cast<ByteUnit>(lit >> 8*(num_bytes[col] - 1 - byte)));
+			// ByteUnit lit_byte = FLIP(static_cast<ByteUnit>(lit >> 8*(num_bytes[col] - 1 - byte)));
+			ByteUnit lit_byte = static_cast<ByteUnit>(lit >> 8*(num_bytes[col] - 1 - byte));
 			// mask_byte[col][byte] = avx_set1(lit_byte);
 			std::cout << "Col#" << col << ", Byte#" << byte << ": " << std::bitset<8>(lit_byte) << std::endl;
 			AvxUnit avx_mask = avx_set1(lit_byte);
@@ -153,17 +154,8 @@ void BytewiseScan::Scan(BitVector* bitvector){
 	        		AvxUnit avx_less = _mm256_lddqu_si256(&m_less[col]);
 	        		AvxUnit avx_greater = _mm256_lddqu_si256(&m_greater[col]);
 	        		AvxUnit avx_equal = _mm256_lddqu_si256(&m_equal[col]);
-	        		std::cout << "GetAvxUnit: " << std::bitset<64>(static_cast<WordUnit>(avx_data[0])) << std::endl;
-	        		std::cout << "data_: "
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0]))
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 8)) 
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 16)) 
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 24)) 
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 32))
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 40)) 
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 48)) 
-	        				<< std::bitset<8>(static_cast<ByteUnit>(avx_data[0] >> 56)) << std::endl; 
-	        		// std::cout << std::bitset<64>(static_cast<WordUnit>(avx_data[1])) << std::endl;
+	        		// std::cout << "GetAvxUnit: " << std::bitset<64>(static_cast<WordUnit>(avx_data[0])) << std::endl;
+
 	        		// std::cout << std::bitset<64>(static_cast<WordUnit>(avx_data[2])) << std::endl;
 	        		// std::cout << std::bitset<64>(static_cast<WordUnit>(avx_data[3])) << std::endl;
 	        		// std::cout << "Before ByteInColumn#"<< j << ": " << _mm256_movemask_epi8(avx_less) << std::endl;

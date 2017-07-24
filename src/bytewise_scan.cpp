7c020afb63ba034cc189db840e135da4c172a2d5
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-24 13:42:57 +0800

    use C++98-styled shuffling

--- src/bytewise_scan.cpp
+++ src/bytewise_scan.cpp
@@ -2,7 +2,6 @@
 #include    <omp.h>
 #include    <vector>
 #include	<algorithm>
-#include	<random>
 
 namespace byteslice{
 
@@ -63,8 +62,7 @@ Sequence BytewiseScan::RandomSequence() const{
 			seq.push_back(ByteInColumn(i, -1));
 		}
 	}
-	auto engine = std::default_random_engine{};
-	std::shuffle(std::begin(seq), std::end(seq), engine);
+	std::random_shuffle(cards_.begin(), cards_.end());
 	return seq;
 }
 

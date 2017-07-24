0a36078254a2640e8bb2341a2fb057bc82b69d57
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-24 13:01:12 +0800

    add test for ValidSequence in bytewise_scan

---
+++ experiments/bytewise_conjunction.cpp
@@ -0,0 +1,44 @@
+#include    <iostream>
+#include    <iomanip>
+#include    <fstream>
+#include    <unistd.h>
+#include    <cstring>
+#include    <cstdlib>
+#include    <ctime>
+
+#include    "include/types.h"
+#include    "include/column.h"
+#include    "include/bitvector.h"
+#include	"include/bytewise_scan.h"
+
+using namespace byteslice;
+
+int main(){
+	size_t num_rows = 1024;
+	Comparator comparator = Comparator::kLess;
+	WordUnit literal = 0ULL;
+
+	Column* column1 = new Column(ColumnType::kByteSlicePadRight, 15, num_rows);
+	Column* column2 = new Column(ColumnType::kByteSlicePadRight, 20, num_rows);
+	Column* column3 = new Column(ColumnType::kByteSlicePadRight, 25, num_rows);
+
+	BytewiseScan scan;
+	scan.AddPredicate(BytewiseAtomPredicate(column1, comparator, literal));
+	scan.AddPredicate(BytewiseAtomPredicate(column1, comparator, literal));
+	scan.AddPredicate(BytewiseAtomPredicate(column1, comparator, literal));
+
+	// generate test case
+	Sequence seq;
+	seq.push_back(ByteInColumn(0, 0));
+	seq.push_back(ByteInColumn(0, 1));
+	seq.push_back(ByteInColumn(1, 0));
+	seq.push_back(ByteInColumn(1, 1));
+	seq.push_back(ByteInColumn(1, 2));
+	seq.push_back(ByteInColumn(2, 0));
+	seq.push_back(ByteInColumn(2, 1));
+	seq.push_back(ByteInColumn(2, 2));
+	seq.push_back(ByteInColumn(2, 3));
+
+	bool valid = scan.ValidSequence(seq);
+	std::cout << valid << std::endl;
+}

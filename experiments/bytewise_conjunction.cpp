0c8b139962a149d3d894c7c611cf1cafd965bd0b
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-24 19:26:14 +0800

    add test for bytewise scan through class bytewise_scan

--- experiments/bytewise_conjunction.cpp
+++ experiments/bytewise_conjunction.cpp
@@ -16,16 +16,44 @@ using namespace byteslice;
 int main(){
 	size_t num_rows = 1024;
 	Comparator comparator = Comparator::kLess;
-	WordUnit literal = 0ULL;
+	WordUnit literal = 0x11111111111111;
 
 	Column* column1 = new Column(ColumnType::kByteSlicePadRight, 15, num_rows);
 	Column* column2 = new Column(ColumnType::kByteSlicePadRight, 20, num_rows);
 	Column* column3 = new Column(ColumnType::kByteSlicePadRight, 25, num_rows);
 
+	// testing Scan
+	std::srand(std::time(0)); //set random seed
+	const WordUnit mask = (1ULL << 15) - 1;
+	for(size_t i = 0; i < num_rows; i++){
+        ByteUnit code = std::rand() & mask;
+        column->SetTuple(i, code);
+    }
+
 	BytewiseScan scan;
 	scan.AddPredicate(BytewiseAtomPredicate(column1, comparator, literal));
-	scan.AddPredicate(BytewiseAtomPredicate(column2, comparator, literal));
-	scan.AddPredicate(BytewiseAtomPredicate(column3, comparator, literal));
+	// scan.AddPredicate(BytewiseAtomPredicate(column2, comparator, literal));
+	// scan.AddPredicate(BytewiseAtomPredicate(column3, comparator, literal));
+
+	BitVector* bitvector1 = new BitVector(num_rows);
+	BitVector* bitvector2 = new BitVector(num_rows);
+	bitvector1->SetOnes();
+    bitvector2->SetOnes();
+	scan.Scan(bitvector1);
+	column1->Scan(comparator, literal, bitvector2, Bitwise::kSet);
+
+	//calculate accuracy
+	size_t corr = 0; //count correct tuples
+	double acc = 0;
+    std::cout << "Scan" << "\t" << "ScanByte" << std::endl;
+    for(size_t i = 0; i < num_rows; i++){ 
+        if(bitvector1->GetBit(i) == bitvector2->GetBit(i)) 
+            corr++; 
+        // std::cout << bitvector2->GetBit(i) << "\t\t" << bm_less->GetByteMask(i) << std::endl;
+    }
+    acc = (double)corr / num_rows;
+    std::cout << "Number of correct tuples: " << corr << std::endl; 
+    std::cout << "Accuracy: " << acc << std::endl;
 
 	// testing ValidSequence
 	// Sequence seq;

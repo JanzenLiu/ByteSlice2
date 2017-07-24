be4115482ce84ebda3b40efc8d3c9b709446644f
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-25 03:10:10 +0800

    modify output

--- experiments/bytewise_conjunction.cpp
+++ experiments/bytewise_conjunction.cpp
@@ -15,8 +15,9 @@
 using namespace byteslice;
 
 int main(){
-	AvxUnit avx = _mm256_set_epi8(0x0, 0x1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
+	// AvxUnit avx = _mm256_set_epi8(0x0, 0x1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	// 	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
+	AvxUnit avx = avx_zero();
 	std::cout << "Manually Set Avx: \n" << std::bitset<64>(static_cast<WordUnit>(avx[0])) << std::endl
 		<< std::bitset<64>(static_cast<WordUnit>(avx[1])) << std::endl
 		<< std::bitset<64>(static_cast<WordUnit>(avx[2])) << std::endl

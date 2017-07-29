6b85f919cd35758ac263849f21efb490ca84d28c
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-29 16:39:45 +0800

    test And, Or, Set functions for byte_mask_block

--- experiments/bytewise.cpp
+++ experiments/bytewise.cpp
@@ -29,14 +29,27 @@ int main(){
     BitVector* bitvector1 = new BitVector(num_rows);
     BitVector* bitvector2 = new BitVector(num_rows);
     (void) bitvector1;
-    ByteMaskBlock* bm_less = new ByteMaskBlock(num_rows);
-    ByteMaskBlock* bm_greater = new ByteMaskBlock(num_rows);
-    ByteMaskBlock* bm_equal = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_result = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_less0 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_greater0 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_equal0 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_less1 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_greater1 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_equal1 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_less2 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_greater2 = new ByteMaskBlock(num_rows);
+    ByteMaskBlock* bm_equal2 = new ByteMaskBlock(num_rows);
     bitvector1->SetOnes();
     bitvector2->SetOnes();
-    bm_less->SetAllFalse();
-    bm_greater->SetAllFalse();
-    bm_equal->SetAllTrue();
+    bm_less0->SetAllFalse();
+    bm_greater0->SetAllFalse();
+    bm_equal0->SetAllTrue();
+    bm_less1->SetAllFalse();
+    bm_greater1->SetAllFalse();
+    bm_equal1->SetAllTrue();
+    bm_less2->SetAllFalse();
+    bm_greater2->SetAllFalse();
+    bm_equal2->SetAllTrue();
     const WordUnit mask = (1ULL << code_length) - 1;
     WordUnit literal = static_cast<WordUnit>(mask * selectivity);
     ByteUnit byte0 = static_cast<ByteUnit>(literal >> 14);
@@ -50,12 +63,19 @@ int main(){
 
 	//single-byte column test
 	// column->ScanByte(comparator, literal, 0, bitvector1, Bitwise::kSet);
-    column->GetBlock(0)->ScanByte(0, comparator, byte0, bm_less, bm_greater, bm_equal);
-    column->GetBlock(0)->ScanByte(1, comparator, byte1, bm_less, bm_greater, bm_equal);
-    column->GetBlock(0)->ScanByte(2, comparator, byte2, bm_less, bm_greater, bm_equal);
+    column->GetBlock(0)->ScanByte(0, comparator, byte0, bm_less0, bm_greater0, bm_equal0);
+    column->GetBlock(0)->ScanByte(1, comparator, byte1, bm_less1, bm_greater1, bm_equal1);
+    column->GetBlock(0)->ScanByte(2, comparator, byte2, bm_less2, bm_greater2, bm_equal2);
 	column->Scan(comparator, literal, bitvector2, Bitwise::kSet);
 
-    bm_less->Condense(bitvector1->GetBVBlock(0));
+    // bm_less->Condense(bitvector1->GetBVBlock(0));
+    bm_less1->And(bm_equal0);
+    bm_equal1->And(bm_equal0);
+    bm_less2->And(bm_equal1);
+    bm_less0->Or(bm_less1);
+    bm_less0->Or(bm_less2);
+    bm_result->Set(bm_less0);
+    bm_result->Condense(bitvector1->GetBVBlock(0));
 
 	//calculate accuracy
 	size_t corr = 0; //count correct tuples

eaf2443e5c1380694e6072cd864f9c724a54a111
Janzen Liu <chengtsung_liu@yahoo.com>
2017-07-30 10:00:16 +0800

    test case with only 2 columns

--- experiments/bytewise_conjunction.cpp
+++ experiments/bytewise_conjunction.cpp
@@ -98,7 +98,7 @@ int main(int argc, char* argv[]){
 	BytewiseScan scan;
 	scan.AddPredicate(BytewiseAtomPredicate(column1, comparator, literal1));
 	scan.AddPredicate(BytewiseAtomPredicate(column2, comparator, literal2));
-	scan.AddPredicate(BytewiseAtomPredicate(column3, comparator, literal3));
+	// scan.AddPredicate(BytewiseAtomPredicate(column3, comparator, literal3));
 	scan.ShuffleSequence();
 	scan.PrintSequence();
 
